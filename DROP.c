f4468f6ed4a22bcb1eda46849c5539b14718af3f
G. Theodor Serbana <theodor.serbana96@gmail.com>
2016-08-19 12:31:11 +0000

    Also wrote working DROP target

---
+++ DROP.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <sys/errno.h>
+#include <stdlib.h>
+#include <string.h>
+#include <libiptc/libiptc.h>
+#include <linux/netfilter/xt_mark.h>
+#include <linux/netfilter/xt_physdev.h>
+#include <linux/netfilter/x_tables.h>
+#include <linux/netfilter/nf_nat.h>
+#include <netinet/in.h>
+#include <unistd.h>
+
+int main()
+{
+        struct xtc_handle *h = iptc_init("filter");
+        int result = 0;
+        if(!h) { printf( "error condition  %s\n", iptc_strerror(errno)); return -1;}
+
+        unsigned int targetOffset =  XT_ALIGN(sizeof(struct ipt_entry));
+
+        unsigned int totalLen = targetOffset + XT_ALIGN(sizeof(struct xt_standard_target));
+
+        struct ipt_entry* e = (struct ipt_entry *)calloc(1, totalLen);
+        if(e == NULL)
+        {
+                printf("calloc failure :%s\n", strerror(errno));
+                return -1;
+        }
+
+        e->target_offset = targetOffset;
+        e->next_offset = totalLen;
+
+		
+		strcpy(e->ip.outiface, "NA_intf1");
+
+        struct xt_standard_target* target = (struct xt_standard_target  *) e->elems;
+        target->target.u.target_size = XT_ALIGN(sizeof(struct xt_standard_target));
+        strcpy(target->target.u.user.name, "DROP");
+		target->target.u.user.revision = 0;
+		target->verdict = -NF_ACCEPT - 1;	
+		
+        int x = iptc_append_entry("OUTPUT", e, h);
+        if (!x)
+        {
+                printf("iptc_append_entry::Error insert/append entry: %s\n", iptc_strerror(errno));
+                result = -1;
+                goto end;
+        }
+
+        int y = iptc_commit(h);
+        if (!y)
+        {
+                printf("iptc_commit::Error commit: %s\n", iptc_strerror(errno));
+                result = -1;
+        }
+
+        end:
+            free(e);
+            iptc_free(h);
+            return result;
+}
